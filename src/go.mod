052f0443e72f97441bae782b1a34bf483f13478d
eisho.uchikakoi <eisho.uchikakoi@broadleaf.co.jp>
2023-06-06 23:35:01 +0900

    url,slackToken,channelIdを環境変数から取得するように変更

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,7 @@
 module status-check-notify-slack/main
 
 go 1.19
+
+require github.com/slack-go/slack v0.12.2
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- src/main.go
+++ src/main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/slack-go/slack"
 	"net/http"
+	"os"
 )
 
-func main() {
-	url := "https://google.com/foo/bar"
+// get Environment Variables URL
+func getUrl() (string, error) {
+	url := os.Getenv("URL")
+	if url == "" {
+		return "", errors.New("URL is not set")
+	}
+	return url, nil
+}
 
+// get Environment Variables SLACK_API_TOKEN
+func getSlackApiToken() (string, error) {
+	slackApiToken := os.Getenv("SLACK_API_TOKEN")
+	if slackApiToken == "" {
+		return "", errors.New("SLACK_API_TOKEN is not set")
+	}
+	return slackApiToken, nil
+}
+
+// get Environment Variables SLACK_API_TOKEN
+func getSlackChannelId() (string, error) {
+	slackChannelId := os.Getenv("SLACK_CHANNEL_ID")
+	if slackChannelId == "" {
+		return "", errors.New("SLACK_CHANNEL_ID is not set")
+	}
+	return slackChannelId, nil
+}
+
+func main() {
+	// check Environment Variables
+	url, err := getUrl()
+	if err != nil {
+		fmt.Printf("get Environment Variables: %s\n", err)
+		return
+	}
+
+	slackApiToken, err := getSlackApiToken()
+	if err != nil {
+		fmt.Printf("get Environment Variables: %s\n", err)
+		return
+	}
+
+	slackChannelId, err := getSlackChannelId()
+	if err != nil {
+		fmt.Printf("get Environment Variables: %s\n", err)
+		return
+	}
+
+	// check status code
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Request error: %v\n", err)
@@ -16,4 +64,24 @@ func main() {
 	defer resp.Body.Close()
 
 	fmt.Printf("Response status: %v\n", resp.Status)
+
+	// status code not 200
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Response error: %v\n", resp.Status)
+
+		// nortiry to slack
+		api := slack.New(slackApiToken)
+		channelID, timestamp, err := api.PostMessage(
+			slackChannelId,
+			slack.MsgOptionText("Hello world!", false),
+		)
+		if err != nil {
+			fmt.Printf("Slack Post Message Error %s\n", err)
+			return
+		}
+
+		fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+
+	}
+
 }
