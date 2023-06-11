9e5ce4d3bb6eec55c66eb7f340cce188ccb96ac7
eisho.uchikakoi <eisho.uchikakoi@broadleaf.co.jp>
2023-06-11 22:32:44 +0900

    オブジェクト作成処理を追加

--- src/go.mod
+++ src/go.mod
@@ -2,6 +2,32 @@ module status-check-notify-slack/main
 
 go 1.19
 
-require github.com/slack-go/slack v0.12.2
+require (
+	cloud.google.com/go/storage v1.30.1
+	github.com/slack-go/slack v0.12.2
+)
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v0.12.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.114.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230320184635-7606e756e683 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"github.com/slack-go/slack"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
+
+	"cloud.google.com/go/storage"
 )
 
 // Config is the configuration for the job.
@@ -14,6 +19,7 @@ type Config struct {
 	url            string // URL
 	slackApiToken  string // SLACK_API_TOKEN
 	slackChannelId string // SLACK_CHANNEL_ID
+	bucketName     string // BUCKET_NAME
 }
 
 // get Environment Variables
@@ -21,12 +27,14 @@ func configFromEnv() (Config, error) {
 	url := os.Getenv("URL")
 	slackApiToken := os.Getenv("SLACK_API_TOKEN")
 	slackChannelId := os.Getenv("SLACK_CHANNEL_ID")
+	bucketName := os.Getenv("BUCKET_NAME")
 
 	// set config from Environment Variables
 	config := Config{
 		url:            url,
 		slackApiToken:  slackApiToken,
 		slackChannelId: slackChannelId,
+		bucketName:     bucketName,
 	}
 
 	// check Environment Variables
@@ -39,8 +47,45 @@ func configFromEnv() (Config, error) {
 	if config.slackChannelId == "" {
 		return config, errors.New("SLACK_CHANNEL_ID is not set")
 	}
+	// if config.bucketName == "" {
+	// 	return config, errors.New("BUCKET_NAME is not set")
+	// }
 
 	return config, nil
+}
+
+// URLをjsonのファイル名に変換する
+func convertURLToFileName(inputURL string) string {
+	// URLを解析
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		// エラーハンドリング
+		panic(err)
+	}
+
+	// ホスト部分を変換
+	host := strings.ReplaceAll(parsedURL.Host, ".", "_")
+
+	// パス部分を変換
+	path := strings.ReplaceAll(parsedURL.Path, "/", "_")
+
+	// ファイル名を組み立て
+	fileName := fmt.Sprintf("%s%s.json", host, path)
+
+	return fileName
+}
+
+// オブジェクトを作成する関数
+func createObject(ctx context.Context, obj *storage.ObjectHandle, data []byte) error {
+	writer := obj.NewWriter(ctx)
+	_, err := writer.Write(data)
+	if err != nil {
+		return err
+	}
+	if err := writer.Close(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -50,6 +95,36 @@ func main() {
 		log.Fatalf("{Environment variables error: %v }\n", err)
 	}
 
+	// config.bucketName が存在する場合は、GCSにアクセスする
+	if config.bucketName != "" {
+		// GCS clientの作成
+		ctx := context.Background()
+		client, err := storage.NewClient(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Close()
+
+		// GCSで使用するオブジェクト名を生成する
+		objectName := convertURLToFileName(config.url)
+		obj := client.Bucket(config.bucketName).Object(objectName)
+
+		// objectが存在するか確認する
+		_, err = obj.Attrs(ctx)
+		if err != nil {
+			if err == storage.ErrObjectNotExist {
+				// オブジェクトが存在しない場合は、作成する
+				err := createObject(ctx, obj, nil)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println("オブジェクトを作成しました")
+			} else {
+				log.Fatal(err)
+			}
+		}
+	}
+
 	// check status code
 	resp, err := http.Get(config.url)
 	if err != nil {
@@ -57,7 +132,7 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	fmt.Printf("{Response: {url: %v, status: %v }}\n",config.url, resp.Status)
+	fmt.Printf("{Response: {url: %v, status: %v }}\n", config.url, resp.Status)
 
 	// status code not 200
 	if resp.StatusCode != http.StatusOK {
